55234a40e16b3c600d31cea9ea70ad1d414f5f65
heinerbehrends <heinerbehrends@hotmail.com>
2018-01-29 00:35:43 +0100

    changed variable name to number_as_string

--- 17check.php
+++ 17check.php
@@ -2,11 +2,11 @@
 // only accepts numbers as strings as input. still figuring out if there's a way to convert the very large number to string using some math extension.
 $test_number  = "92651109849427955";
 
-function getResultsArray($very_large_number) {
+function getResultsArray($number_as_string) {
   $digits_array = array();
-  $j = strlen($very_large_number);
-  for ($i = 0; $i < strlen($very_large_number); $i++) {
-    $digits_array[$i] = $very_large_number[$i] * $j;
+  $j = strlen($number_as_string);
+  for ($i = 0; $i < strlen($number_as_string); $i++) {
+    $digits_array[$i] = $number_as_string[$i] * $j;
     $j--;
   }
   return $digits_array;
@@ -20,8 +20,8 @@ function getSumOfResults($resultArray) {
   return $sum;
 }
 
-function checkNumber($very_large_number) {
-  return ! (getSumOfResults(getResultsArray($very_large_number)) % 17);
+function checkNumber($number_as_string) {
+  return ! (getSumOfResults(getResultsArray($number_as_string)) % 17);
 }
 
 echo (checkNumber($test_number) ? "TRUE" : "FALSE") ;
